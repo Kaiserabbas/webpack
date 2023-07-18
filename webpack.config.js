78ee1ed0ff6e37a76d9dcc2aea777afc943983b5
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-07-18 14:24:13 +0500

    Load CSS

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,29 @@ module.exports = {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
